2188776d4df1e0a47f6ed008dd783d2053e80cd3
Federico Nocetti <federiconocetti@Federicos-MacBook-Pro-2.local>
2023-10-08 00:29:15 -0300

    Books list page design

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { CacheProvider, EmotionCache } from '@emotion/react';
 import { CssBaseline } from '@mui/material';
 import type { AppProps } from 'next/app'
 import Head from 'next/head';
+import '../styles/global.css';
 
 const clientSideEmotionCache = createEmotionCache();
 
@@ -14,7 +15,12 @@ export default function MyApp({ Component, pageProps, emotionCache = clientSideE
   return (
     <CacheProvider value={emotionCache}>
       <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <meta
+            name="description"
+            content="Web library for the Gutenberg Project"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <CssBaseline />
       <Component {...pageProps} />
