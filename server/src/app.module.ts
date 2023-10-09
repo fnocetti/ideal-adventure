b594a8c7e7577a2898f0d1b808da24082b9468c9
Federico Nocetti <federiconocetti@Federicos-MacBook-Pro-2.local>
2023-10-08 21:53:27 -0300

    Create favorites module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { FavoritesModule } from './favorites/favorites.module';
 
-@Module({})
+@Module({
+  imports: [FavoritesModule],
+})
 export class AppModule {}
