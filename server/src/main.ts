7a6b9758b5e836a883e2f273dde62ccf6c746d2e
Federico Nocetti <federiconocetti@Federicos-MacBook-Pro-2.local>
2023-10-10 00:10:21 -0300

    Sever can configure port

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3004);
+  const port = parseInt(process.env.PORT ?? '3004');
+  await app.listen(port);
 }
 bootstrap();
